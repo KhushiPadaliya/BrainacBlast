160b9dc8b2649e04206c03fa8c613459edaf0af2
PreetPadaliya <preetpadaliya2805@gmail.com>
2024-04-20 12:56:43 +0530

    add

---
+++ brainybunch/src/componant/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function Login ()
+{
+
+return(
+    <>
+
+<input type="text" placeholder="Username" />
+
+<input type="password" placeholder="Password" />
+<button>Login</button>
+
+
+
+    </>
+)
+
+
+}
+export default Login; 
